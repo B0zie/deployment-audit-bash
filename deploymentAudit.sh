aebde3406ea18b72bdd74067288e1f4a6236e421
Robert Fisher <rfbgz@umsl.edu>
2026-04-25 15:45:17 -0500

    created an audit function that searches through the sub-dirs and counts the file extension prints them to the console and to a file

--- deploymentAudit.sh
+++ deploymentAudit.sh
@@ -3,24 +3,41 @@
 
 
 #Task 1
+#Note there is some extra logic because i wanted to upload to git
+#and i had to find a way to ignore the repo files
 function fileTypeCount() {
-   declare -a count
-   #Needs to not search through .git
-   #needs to not include my script in the sum
-   for f in $(find . -type f); do
-     #fileType=${f##*\.}
-     #echo $fileType
-     echo $f   
-   done
+  countArr=()
+  typeArr=()
+  fList=""
+   for d in ./* ; do
+      dirName=${d##*\.}
+      if [[ $d != ".git" && -d $d ]] ; then
+          for f in $(find $d/* -type f); do
+             fileType=${f##*\.}
+             typeArr+=(".$fileType")
+          done
+      fi
+    done
+    
 
- 
+    for ((i=0; i < ${#typeArr[*]}; i++)); do
+       lastIt=(${#typeArr[*]}-1)
+       if [[ $i -eq $lastIt ]]; then
+          fList+="${typeArr[i]}"
+       else
+          fList+="${typeArr[i]}\n"
+       fi
+    done
+    echo "=====File Audit====="
+    echo -e $fList|sort|uniq -c|sort -n 
+       
 }
 
 
 #MENU
 echo "MENU"
 echo "======"
-echo "1) Count file types"
+echo "1)Audit file types"
 echo "2)"
 echo "3)"
 read -p "Choose: " choice
@@ -28,6 +45,7 @@ read -p "Choose: " choice
 case $choice in
   1)
       fileTypeCount
+      (fileTypeCount)> task1Output.txt
   ;;
   *)
    echo Some menu options are not available
