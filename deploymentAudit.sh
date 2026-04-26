4a52c4304f96ba03104588865e40b7e0881d1eb5
Robert Fisher <rfbgz@umsl.edu>
2026-04-26 14:45:16 -0500

    added scan function that flags files taht have suspicious names or extensions

--- deploymentAudit.sh
+++ deploymentAudit.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #Auther:Robert Fisher
+exitProgram=false
+
 
 
 #Task 1
@@ -33,22 +35,67 @@ function fileTypeCount() {
        
 }
 
+#Task2
+function fileScan() {
+warnList=()
+   for d in ./* ; do
+        dirName=${d##*\.}
+        #if the dir is not git we do find 
+        if [[ $d != ".git" && -d $d ]] ; then
+            for f in $(find $d/* -type f); do
+                fileName=${f##*/}
+               if [[ $(echo $f | grep 'debug') ]];then
+                 warnList+=("$dirName/$fileName")
+               elif [[ $(echo $f | grep '.bak') || $(echo $f | grep '.old') || $(echo $f | grep '.tmp') ]];then
+                 warnList+=("$dirName/$fileName")
+               fi
+            done
+        fi
+   done
+   echo "==Suspicious Files======"
+   echo ${warnList[*]}|tr " " "\n"|awk '{print "WARNING: suspicious file detected - " $0}'
 
-#MENU
-echo "MENU"
-echo "======"
-echo "1)Audit file types"
-echo "2)"
-echo "3)"
-read -p "Choose: " choice
+}
 
-case $choice in
-  1)
-      fileTypeCount
-      (fileTypeCount)> task1Output.txt
-  ;;
-  *)
-   echo Some menu options are not available
-  ;;
-esac
 
+
+
+#Main Program LOOP
+until [[ $exitProgram == true ]]; do
+   #MENU
+   echo "MENU"
+   echo "======"
+   echo "1) Audit file types"
+   echo "2) Scan for Suspicious Files"
+   echo "3) Scan for Dangerous commands "
+   echo "4) Scan for traffic/error patterns "
+   echo "5) Scan for secrets "
+   echo "Q) Hit Q to Quit "
+   read -p "Choose: " choice
+
+   case $choice in
+      1)
+         fileTypeCount
+         (fileTypeCount)> task1Output.txt
+      ;;
+      2) 
+         fileScan
+         (fileScan)> task2Output.txt 
+      ;;
+      3)
+         echo "Option unavailable :("
+      ;;
+      4)
+         echo "Option unavailable :("
+      ;;
+      5)
+         echo "Option unavailable :("
+      ;;
+      [Qq])
+         exitProgram=true
+      ;;
+      *)
+        echo Some menu options are not available
+      ;;
+   esac
+done
